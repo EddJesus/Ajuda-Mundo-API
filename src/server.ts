96b068ab689435d61c8ea98423ef5c4d6d0143a9
Lucas Nadai (Ecogen Brasil) <lucasnadai10@gmail.com>
2022-05-30 23:28:35 -0300

    refactor: remove console.log

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,6 @@ import { app } from './app'
 
 const PORT = process.env.PORT
 
-console.log('-----------------')
-console.log(PORT)
-console.log('-----------------')
-
 app.listen(PORT, () => {
   console.log(`server rodando na porta ${PORT}`)
 })
