107b19edf1999a69707854b2300a4615cb09c725
EddJesus <edusanto22@gmail.com>
2022-06-08 01:14:52 -0300

    feat: create ActivityEntity

---
+++ src/entities/Activity.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+@Entity()
+export class Activities extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    activity_id: number
+
+    @Column()
+    name: string
+
+    @Column()
+    points: number
+
+    @Column({ nullable: true })
+    description: string
+
+    @Column({ name: 'main_img', nullable: true })
+    mainImg: number
+
+    @Column({ default: 0 })
+    status: number
+
+    @Column({ name: 'ong_id' })
+    ongId: number
+
+    @Column({ name: 'user_id' })
+    userId: number
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date
+}
