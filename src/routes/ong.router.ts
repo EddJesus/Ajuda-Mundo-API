d65dfd4c08f17e0665e619661a8ca54f7d655001
EddJesus <edusanto22@gmail.com>
2022-06-11 04:35:11 -0300

    feat: ong crud routes

--- src/routes/ong.router.ts
+++ src/routes/ong.router.ts
@@ -1,11 +1,25 @@
 import { Router, Request, Response } from 'express'
 
+import { makeValidateBody } from '../middlewares/RouteValidator'
+
+import { OngFactory, CreateOngDto } from '../modules/Ong'
+
 const routes = Router()
 
-routes.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
-    status: 'up',
-  })
+routes.get('/', async (req: Request, res: Response) => {
+  await OngFactory().findAllOngs(req, res)
 })
+
+routes.get('/:id', async (req: Request, res: Response) => {
+  await OngFactory().findOngById(req, res)
+})
+
+routes.post(
+  '/',
+  makeValidateBody(CreateOngDto),
+  async (req: Request, res: Response) => {
+    await OngFactory().createOng(req, res)
+  },
+)
 
 export { routes }
