06dde5382bc0050152ac9f1ec4e320ffb2ce6830
EddJesus <edusanto22@gmail.com>
2022-06-11 04:02:15 -0300

    feat: user crud routes

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,11 +1,26 @@
 import { Router, Request, Response } from 'express'
 
+import { makeValidateBody } from '../middlewares/RouteValidator'
+
+import { UserFactory, CreateUserDto } from '../modules/User'
+
 const routes = Router()
 
-routes.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
-    status: 'up',
-  })
+routes.get('/', async (req: Request, res: Response) => {
+  await UserFactory().findAllUsers(req, res)
 })
+
+routes.get('/:id', async (req: Request, res: Response) => {
+  console.log('caindo aqui')
+  await UserFactory().findUserById(req, res)
+})
+
+routes.post(
+  '/',
+  makeValidateBody(CreateUserDto),
+  async (req: Request, res: Response) => {
+    await UserFactory().createUser(req, res)
+  },
+)
 
 export { routes }
